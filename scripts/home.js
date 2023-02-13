1e2b72053be6f6d79d08615e0f87a7b7f7061f9e
Nicolas Klaumann <nicolas@conectra.com.br>
2023-02-12 23:54:15 -0300

    feat: adiciona query de 3 cards

---
+++ scripts/home.js
@@ -0,0 +1,55 @@
+
+const total_funcionarios = document.querySelector('.total_funcionarios');
+const empresa_mais_antiga = document.querySelector('.empresa_mais_antiga');
+const total_setor = document.querySelector('.total_setor');
+
+const queryDois = async () => {
+    const res = await _supabase.rpc('empresa_mais_antiga');
+
+    if (res) {
+        console.log(res.data);
+        empresa_mais_antiga.innerHTML = `${res.data}`
+    }
+}
+
+const queryQuatro = async () => {
+    // busca o total de cada setor
+    const industrial = await _supabase.rpc('get_industrial');
+    const varejo = await _supabase.rpc('get_varejo');
+    const servicos = await _supabase.rpc('get_servicos');
+    const agricola = await _supabase.rpc('get_agricola');
+
+    let setores = [
+        ['INDUSTRIAL', industrial.data],
+        ['VAREJO', varejo.data],
+        ['SERVIÇOS', servicos.data],
+        ['AGRICOLA', agricola.data]
+    ];
+
+    setores.sort()
+    setores.reverse()
+
+    total_setor.innerHTML = `
+        <p>${setores[0][0]} - ${setores[0][1]} Empresas</p><br>
+        <p>${setores[1][0]} - ${setores[1][1]} Empresas</p><br>
+        <p>${setores[2][0]} - ${setores[2][1]} Empresas</p><br>
+        <p>${setores[3][0]} - ${setores[3][1]} Empresas</p><br>
+    `
+}
+
+const queryCinco = async () => {
+    const res = await _supabase.rpc('total_funcionarios');
+    if(res) {
+        console.log(res.data);
+        total_funcionarios.innerHTML = `${res.data}`
+    }
+
+}
+
+const montaTela = async () => {
+    queryDois();
+    queryQuatro();
+    queryCinco();
+}
+
+montaTela()
